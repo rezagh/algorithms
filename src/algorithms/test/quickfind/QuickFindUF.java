841485398727b72649481b9641b40d5f291ba44c
Reza Ghafari <rezaghp@gmail.com>
2013-10-22 08:45:17 +1100

    1

--- src/algorithms/test/quickfind/QuickFindUF.java
+++ src/algorithms/test/quickfind/QuickFindUF.java
@@ -7,29 +7,34 @@ package algorithms.test.quickfind;
  * 		whose id equals id[p] to id[q]
  * 
  * index 	0	1	2	3	4
- * id[]		0	1	2	3	4	initia
+ * id[]		0	1	2	3	4	initially
  * 
- * id[]		0	1	2	3	2	2,4 are connected
+ * id[]		0	1	4	3	4	union 2,4 (2,4 are connected)
  *							
  * id[]		0	1	3	3	3	connect 2,3		
+ * 
+ * this algo is N2 (N to the power of 2)
  */
 public class QuickFindUF {
 
 	private int [] id;
 	
-	private QuickFindUF(int n){
+	public QuickFindUF(int n){
+        id = new int[n];
 		for(int i=0;i<n;i++)
 			id[i]=i;
 	}
 	
 	public boolean connected(int p, int q){
-		if(id[p]==id[q]) return true;
-		return false;
+		return id[p]==id[q];
 	}
 	
 	public void union(int p, int q){
+		int idp = id[p];
+		int idq = id[q];
 		for(int i = 0; i < id.length ; i++){
-			if(id[i]==id[p])
+			if(id[i]==idp)
+				id[i]=idq;
 		}
 	}
 
